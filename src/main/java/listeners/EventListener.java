3833a0f1d27901ef00d6aeb96aa9ad7b39e8f399
fastgogame <thefastgogame@gmail.com>
2023-08-06 04:53:09 +0600

    manifest

--- src/main/java/listeners/EventListener.java
+++ src/main/java/listeners/EventListener.java
@@ -1,5 +1,6 @@
 package listeners;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.openqa.selenium.By;
@@ -9,11 +10,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class EventListener extends ListenerAdapter {
-
+//    ChromeOptions options = new ChromeOptions();
+//    options.addArguments("--disable-notifications");
     WebDriver driver = new ChromeDriver();
     public void startServer(){
         driver.get("https://ploudos.com/manage/0/");
-        //css-k8o10q
         WebElement acceptCookies = driver.findElement(By.className("css-k8o10q"));
         acceptCookies.click();
         WebElement startButton = driver.findElement(By.className("btn-success"));
@@ -28,18 +29,22 @@ public class EventListener extends ListenerAdapter {
             if (authCheck != null) {
                 startServer();
             } else {
+
                 driver.get("https://ploudos.com/login/");
                 WebElement usernameField = driver.findElement(By.name("username"));
                 WebElement passwordField = driver.findElement(By.name("password"));
                 WebElement loginButton = driver.findElement(By.xpath("//button[text()='Log In']"));
 
-                usernameField.sendKeys("fastgogame");
-                passwordField.sendKeys("MmAqebQTrL");
+                Dotenv config = Dotenv.configure().load();
+                String login = config.get("LOGIN");
+                String password = config.get("PASSWORD");
+                usernameField.sendKeys(login);
+                passwordField.sendKeys(password);
                 loginButton.click();
 
                 startServer();
             }
-            event.getChannel().sendMessage("Server started!").queue();
+            event.getChannel().sendMessage("Server is starting").queue();
             driver.quit();
         }
     }
